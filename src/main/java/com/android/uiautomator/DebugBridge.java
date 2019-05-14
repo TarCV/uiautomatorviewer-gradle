5eec9e5cedfa43405066a9f1431dcbaf59ca24cf
TarCV <TarCV@users.noreply.github.com>
2019-05-14 20:12:51 +0300

    Initial commit

---
+++ src/main/java/com/android/uiautomator/DebugBridge.java
@@ -0,0 +1,97 @@
+/*
+ * Original code is copyright (C) 2012 The Android Open Source Project
+ * New code and modifications are Copyright (C) 2019 TarCV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.uiautomator;
+
+import com.android.ddmlib.AndroidDebugBridge;
+import com.android.ddmlib.IDevice;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class DebugBridge {
+    private static AndroidDebugBridge sDebugBridge;
+
+    private static String getAdbLocation() {
+        File sdk;
+        String toolsDir = System.getProperty("com.android.uiautomator.bindir"); //$NON-NLS-1$
+
+        if (toolsDir == null) {
+            String sdkDir = System.getenv("ANDROID_HOME");
+            if (sdkDir == null) {
+                sdkDir = System.getProperty("sdk.dir");
+
+                if (sdkDir == null) {
+                    return null;
+                }
+            }
+
+            sdk = new File(sdkDir);
+        } else {
+            sdk = new File(toolsDir).getParentFile();
+            toolsDir = new File(sdk, "tools").getAbsolutePath();
+        }
+
+        // check if adb is present in platform-tools
+        File platformTools = new File(sdk, "platform-tools");
+        File adb = findAdbIn(platformTools);
+        if (adb != null) {
+            return adb.getAbsolutePath();
+        }
+
+        // check if adb is present in the tools directory
+        adb = findAdbIn(new File(toolsDir));
+        if (adb != null) {
+            return adb.getAbsolutePath();
+        }
+
+        return null;
+    }
+
+    private static File findAdbIn(File platformTools) {
+        File[] files = platformTools.listFiles((dir, name) -> "adb".equals(name) || name.startsWith("adb."));
+        if (files != null && files.length > 0) {
+            return files[0];
+        } else {
+            return null;
+        }
+    }
+
+    public static void init() {
+        String adbLocation = getAdbLocation();
+        if (adbLocation != null) {
+            AndroidDebugBridge.init(false /* debugger support */);
+            sDebugBridge = AndroidDebugBridge.createBridge(adbLocation, false);
+        }
+    }
+
+    public static void terminate() {
+        if (sDebugBridge != null) {
+            sDebugBridge = null;
+            AndroidDebugBridge.terminate();
+        }
+    }
+
+    public static boolean isInitialized() {
+        return sDebugBridge != null;
+    }
+
+    public static List<IDevice> getDevices() {
+        return Arrays.asList(sDebugBridge.getDevices());
+    }
+}
