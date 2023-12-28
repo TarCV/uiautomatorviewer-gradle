b9339c9b7f60471faad81120959b9e389bb90096
TarCV <tarcv@users.noreply.github.com>
2023-12-28 07:00:46 +0300

    Finalize/cleanup JDK 21 support

--- shell.nix
+++ shell.nix
@@ -1,20 +1,4 @@
-with import <nixpkgs> { overlays = [
-  (self: super:
-    let
-      # TODO: remove when JDK21 support is merged in
-      jdk21Pkgs = import (builtins.fetchTarball {
-        name = "jdk21Pkgs";
-        url = "https://github.com/NixOS/nixpkgs/archive/9a00ebc79fbdc9bed1964af8756ebec95af8191c.tar.gz";
-        #        # Use nix-prefetch-url --unpack to generate:
-        sha256 = "0rz45pyg1mnviw6l9vv65w84jpqzx84z3r78k1v77ciymjd5fdg9";
-      }) {};
-    in
-    {
-      jdk21 = jdk21Pkgs.jdk21;
-   }
-  )
-];};
-
+with import <nixpkgs> {};
 (pkgs.buildFHSUserEnv {
   name = "uiautomatorviewer-env";
   targetPkgs = pkgs: (with pkgs;
